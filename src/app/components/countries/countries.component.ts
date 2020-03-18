a9529822aa612040b8df7d59ff13e1ba4c12772d
Andrew <a9052681569@gmail.com>
2020-03-18 15:24:19 +0300

    добавил плавную анимацию добавления и удаления пунктов в компоненте countries

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { Country } from 'src/app/types/country/country';
 import { CountryDataService } from 'src/app/services/country-data/country-data.service';
 import { softAppear } from 'src/app/animations/soft-appear/soft-appear.animation';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-countries',
@@ -15,25 +17,36 @@ export class CountriesComponent implements OnInit {
   public countryForm: FormGroup
   public countries: Country[]
 
-
   constructor(private dataService: CountryDataService, private formBuilder: FormBuilder) {
     this.countryForm = formBuilder.group({
-      name: ''
+      name: [null, [Validators.required, Validators.pattern(/^[а-яё\s\-]+$/i)]]
     })
   }
 
-  public addCountry(): void {
-    this.dataService.addData(this.countryForm.value.name)
-    this.countryForm.reset()
+  private addCountry(): void {
+    this.dataService.addData(this.countryForm.get('name').value)
   }
   public deleteCountry(id: number): void {
     this.dataService.deleteData(id)
   }
-  public animationLog(s: AnimationEvent) {
+  public animationLog(s: AnimationEvent): void {
     console.log('анимируем')
+  }
+  public submit() {
+    if(this.countryForm.valid) {
+      this.addCountry()
+      this.countryForm.reset()
+    } else {
+      this._name.markAsTouched()
+    }
   }
   ngOnInit(): void {
     this.countries = this.dataService.getData()
+  }
+  
+
+  get _name(): AbstractControl {
+    return this.countryForm.get('name')
   }
   
 }
